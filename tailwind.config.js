69fa8f211a0180d059427659bec9876636e8e594
Andrew Usher <andrewusher00@gmail.com>
2022-09-11 19:48:18 -0500

    feat: add mobile-only variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+// eslint-disable-next-line
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  content: ['./app/**/*.{ts,tsx,jsx,js,mdx}'],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-};
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+    plugin(function ({ addVariant }) {
+      addVariant(
+        'mobile-only',
+        "@media screen and (max-width: theme('screens.sm'))"
+      )
+    }),
+  ],
+}
