c2c343de3c28073ffe0ca8a51f3b28b2a0594cc0
Andrew Usher <andrewusher00@gmail.com>
2022-10-20 13:56:27 -0500

    chore: update content

--- app/constants/showInfo.ts
+++ app/constants/showInfo.ts
@@ -1,4 +1,4 @@
-export const podcastDescription = `901 Speaks is a podcast from a bar regular in downtown Memphis.
-Hosted by Andrew Usher, the podcast has been designed to tell
+export const podcastDescription = `901 Speaks is a podcast from a couple of bar regulars in downtown Memphis.
+Hosted by Andrew Usher and Austin Wiley, the podcast has been designed to tell
 stories and gain insights of people in the downtown Memphis
 community, service industry or regular patron.`
